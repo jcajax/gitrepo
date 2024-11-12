3f75893750083785c8a3d948a6374ed3282a616b
jcajax <yuchen.j2009@gmail.com>
2024-11-12 02:10:20 -0600

    test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+# import cv2
+# import math
+# import numpy as np
+
+#b=4
+print("hello world!")
+# print('Dad!!')
+# print("Mom!!")
+# print(cv2.getVersionString())
+# print(math.sqrt(b))
+# image = cv2.imread('opencv_logo.jpg')
+# print(image.shape)
+# cv2.imshow('image', image)
+# cv2.waitKey()
+# n = np.random.rand(2,4)
+# print(n)
